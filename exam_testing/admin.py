5a3ab48b16939348971ebab811fe3fb36556798e
ninza <ninza147258@gmail.com>
2022-07-05 23:48:21 +0300

    model changed

--- exam_testing/admin.py
+++ exam_testing/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Question, QuestionAnswer, TestQuestions, Test, UserAttempt, User, QuestionUser
+from .models import Question, QuestionAnswer, Test, UserAttempt, User, QuestionUser
 
 admin.site.register(Question)
 admin.site.register(QuestionAnswer)
-admin.site.register(TestQuestions)
 admin.site.register(Test)
 admin.site.register(UserAttempt)
 admin.site.register(User)
